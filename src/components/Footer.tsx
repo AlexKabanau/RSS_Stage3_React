b29642f36768639e9287ba86eac29a761273c74f
Alexandr Kabanau <azraill1989@gmail.com>
2025-01-31 14:01:22 +0300

    feat: add basit marckdown and footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,34 @@
+import { Component } from 'react';
+// import { ReactComponent as GithubSvg } from '../assets/github_logo_black.svg';
+import gitHubLogo from '../assets/github_logo_black.svg';
+import RSSLogo from '../assets/rs_logo_black.svg';
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <div className="creator">
+          <a
+            // className="tooltips"
+            href="https://github.com/AlexKabanau"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img className="creator__link" src={gitHubLogo} alt="github logo" />
+            <span>AlexKabanau</span>
+          </a>
+        </div>
+        <div className="rsschool">
+          <a
+            href="https://rs.school/courses/reactjs"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {/* <GithubSvg /> */}
+            <img className="rsschool__link" src={RSSLogo} alt="RSSchool logo" />
+          </a>
+        </div>
+      </footer>
+    );
+  }
+}
