3281cbb699b2e9eef7d1550627d69fc9f1f251f9
Alexandr Kabanau <azraill1989@gmail.com>
2025-01-31 15:52:28 +0300

    add: add some stiles for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,36 @@
 import { Component } from 'react';
+import styles from './Header.module.css';
+import SearchInput from './SearchInput';
 
 export default class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <header className={styles.header_bordered}>
+        {/* Header */}
+        {/* <Container className="flex items-center justify-between py-8"> */}
+        {/* левая часть */}
+        <div className={styles.search_container}>
+          <SearchInput />
+        </div>
+        <button
+        //  onClickSignIn={() => setOpenAuthModal(true)}
+        >
+          Search Button
+        </button>
+        {/* правая часть */}
+        {/* <div className="flex items-center gap-3">
+            <AuthModal
+              open={openAuthModal}
+              onClose={() => {
+                setOpenAuthModal(false);
+              }}
+            />
+
+            <ProfileButton onClickSignIn={() => setOpenAuthModal(true)} />
+            {hasCart && <CartButton />}
+          </div> */}
+        {/* </Container> */}
+      </header>
+    );
   }
 }
