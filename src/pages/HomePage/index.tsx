16cba60dbf86fb80aafbf4461cdf487869bb8f5d
Alexandr Kabanau <azraill1989@gmail.com>
2025-02-07 14:52:21 +0300

    fix: homePage  into function component

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,5 +1,49 @@
-import React from 'react';
+import { useState } from 'react';
+import Header from '../../components/Header';
+import reactLogo from '../../assets/react.svg';
+import Main from '../../components/Main';
+import ErrorButton from '../../components/ErrorButton';
+import { ResponseType } from '../../api/getItems';
+import Footer from '../../components/Footer';
+// import Footer from '../../components/Footer';import { ResponseType } from './api/getItems';
 
 export default function HomePage() {
-  return <div>HomePage</div>;
+  const [data, setData] = useState<ResponseType[] | null>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
+
+  // const setIsLoading = (isLoading: boolean) => {
+  //   setIsLoading(isLoading);
+  // };
+
+  // const setIsError = (isError: boolean) => {
+  //   setIsError(isError);
+  // };
+
+  // const setData = (data: ResponseType[]) => {
+  //   setData(data);
+  // };
+
+  return (
+    <div className="app">
+      <Header
+        handleResponse={setData}
+        setIsLoading={setIsLoading}
+        setIsError={setIsError}
+      />
+      {isLoading && (
+        <div>
+          <p>Loading...</p>
+          <img src={reactLogo} className="logo" alt="loading" />
+        </div>
+      )}
+      {isError && <div>Some error occurred. Please try again.</div>}
+      {data && <Main items={data} />}
+      <div className="error-container">
+        <ErrorButton />
+      </div>
+
+      <Footer />
+    </div>
+  );
 }
