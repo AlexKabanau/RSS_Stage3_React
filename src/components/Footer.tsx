a3f7b7e8334704d56060eb3c7afd66567c5d1a19
Alexandr Kabanau <azraill1989@gmail.com>
2025-02-07 14:55:00 +0300

    fix: fix footer into FC

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,31 +1,29 @@
-import { Component } from 'react';
+import { Link } from 'react-router';
 import gitHubLogo from '../assets/github_logo_black.svg';
 import RSSLogo from '../assets/rs_logo_black.svg';
 
-export default class Footer extends Component {
-  render() {
-    return (
-      <footer className="footer">
-        <div className="creator">
-          <a
-            href="https://github.com/AlexKabanau"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img className="creator__link" src={gitHubLogo} alt="github logo" />
-            <span>AlexKabanau</span>
-          </a>
-        </div>
-        <div className="rsschool">
-          <a
-            href="https://rs.school/courses/reactjs"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img className="rsschool__link" src={RSSLogo} alt="RSSchool logo" />
-          </a>
-        </div>
-      </footer>
-    );
-  }
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="creator">
+        <Link
+          to="https://github.com/AlexKabanau"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img className="creator__link" src={gitHubLogo} alt="github logo" />
+          <span>AlexKabanau</span>
+        </Link>
+      </div>
+      <div className="rsschool">
+        <Link
+          to="https://rs.school/courses/reactjs"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img className="rsschool__link" src={RSSLogo} alt="RSSchool logo" />
+        </Link>
+      </div>
+    </footer>
+  );
 }
