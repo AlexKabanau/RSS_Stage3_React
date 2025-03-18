eaa09e3025676ec59abe81d097a9b564593b8971
AlexKabanau <azraill1989@gmail.com>
2025-03-18 16:08:14 +0300

    add: add search input fix filter

---
+++ src/components/Search.tsx
@@ -0,0 +1,89 @@
+import React, { ChangeEvent, useRef, MouseEvent } from 'react';
+// import debounce from 'lodash.debounce';
+import { useSelector, useDispatch } from 'react-redux';
+// import { useContext } from "react";
+
+// import style from './Search.module.scss';
+// import { SearchContext } from "../../App";
+import { useCallback } from 'react';
+import { useState } from 'react';
+import { setSearchValue } from '../redux/slices/filterSlice';
+import { useAppDispatch } from '../redux/store';
+// import { setSearchValue } from '../../redux/slices/filterSlice.ts';
+
+const Search: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const [value, setValue] = useState('');
+  // const { setSearchValue } = useContext(SearchContext);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const onClickClear = (event: MouseEvent<SVGSVGElement>) => {
+    dispatch(setSearchValue(''));
+    setValue('');
+    // setSearchValue("");
+
+    ///
+    // if (inputRef.current) {
+    //   inputRef.current.focus();
+    // }
+
+    ///
+    inputRef.current?.focus();
+  };
+
+  // const updateSearchValue = useCallback(
+  //   debounce((str: string) => {dispatch(setSearchValue(str));}, 250),
+  //   []
+  // );
+  const updateSearchValue = useCallback((str: string) => {
+    dispatch(setSearchValue(str));
+  }, []);
+
+  const onChangeInput = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+    updateSearchValue(event.target.value);
+  };
+
+  return (
+    <div className={'style.root'}>
+      <svg
+        className={'style.icon'}
+        enableBackground="new 0 0 32 32"
+        id="Glyph"
+        version="1.1"
+        viewBox="0 0 32 32"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M27.414,24.586l-5.077-5.077C23.386,17.928,24,16.035,24,14c0-5.514-4.486-10-10-10S4,8.486,4,14  s4.486,10,10,10c2.035,0,3.928-0.614,5.509-1.663l5.077,5.077c0.78,0.781,2.048,0.781,2.828,0  C28.195,26.633,28.195,25.367,27.414,24.586z M7,14c0-3.86,3.14-7,7-7s7,3.14,7,7s-3.14,7-7,7S7,17.86,7,14z"
+          id="XMLID_223_"
+        />
+      </svg>
+      <input
+        ref={inputRef}
+        value={value}
+        // onChange={(event) => setSearchValue(event.target.value)}
+        onChange={onChangeInput}
+        className={'style.input'}
+        type="text"
+        placeholder="Поиск пиццы...."
+      />
+      {value && (
+        <svg
+          onClick={onClickClear}
+          className={'style.clearIcon'}
+          height="512px"
+          id="Layer_1"
+          version="1.1"
+          viewBox="0 0 512 512"
+          width="512px"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M437.5,386.6L306.9,256l130.6-130.6c14.1-14.1,14.1-36.8,0-50.9c-14.1-14.1-36.8-14.1-50.9,0L256,205.1L125.4,74.5  c-14.1-14.1-36.8-14.1-50.9,0c-14.1,14.1-14.1,36.8,0,50.9L205.1,256L74.5,386.6c-14.1,14.1-14.1,36.8,0,50.9  c14.1,14.1,36.8,14.1,50.9,0L256,306.9l130.6,130.6c14.1,14.1,36.8,14.1,50.9,0C451.5,423.4,451.5,400.6,437.5,386.6z" />
+        </svg>
+      )}
+    </div>
+  );
+};
+
+export default Search;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,17 @@
 import React, { useEffect, useRef, useState } from 'react';
 import CountryBlock from '../components/CountryBlock';
+import Search from '../components/Search';
+import { selectSort, setSort } from '../redux/slices/filterSlice';
+import { useAppDispatch } from '../redux/store';
+import { useSelector } from 'react-redux';
 
 type SortItem = {
   name: string;
   sortProperty: SortPropertyEnum;
+};
+type FilterItem = {
+  name: string;
+  filterProperty: FilterPropertyEnum;
 };
 
 export enum SortPropertyEnum {
@@ -12,29 +20,97 @@ export enum SortPropertyEnum {
   NAME_DESC = 'name',
   NAME_ASC = '-name',
 }
+export enum FilterPropertyEnum {
+  ALL = 'All',
+  ANTARCTIC = 'Antarctic',
+  AMERICAS = 'Americas',
+  EUROPE = 'Europe',
+  AFRICA = 'Africa',
+  ASIA = 'Asia',
+  OCEANIA = 'Oceania',
+}
 
 function HomePage() {
+  const dispatch = useAppDispatch();
+
   // const { categoryId, sort, currentPage, searchValue } = useAppSelector(selectFilter);
-  const onChangeCategory = (categoryId: number) => {
-    console.log(categoryId);
-    // dispatch(setCategoryId(categoryId));
-  };
+  // const onChangeCategory = (categoryId: number) => {
+  //   console.log(categoryId);
+  //   // dispatch(setCategoryId(categoryId));
+  // };
   //TODO dropdown
-  const categories = [
-    'All',
-    'Antarctic',
-    'Americas',
-    'Europe',
-    'Africa',
-    'Asia',
-    'Oceania',
+  // const categories = [
+  //   'All',
+  //   'Antarctic',
+  //   'Americas',
+  //   'Europe',
+  //   'Africa',
+  //   'Asia',
+  //   'Oceania',
+  // ];
+
+  //NEW CAT FILTER
+  const filterRef = useRef<HTMLDivElement>(null);
+  const [openFilter, setOpenFilter] = useState(false);
+  // const sort = useAppSelector(selectSort);
+  const filterList: FilterItem[] = [
+    {
+      name: 'All',
+      filterProperty: FilterPropertyEnum.ALL,
+    },
+    {
+      name: 'Antarctic',
+      filterProperty: FilterPropertyEnum.ANTARCTIC,
+    },
+    {
+      name: 'Americas',
+      filterProperty: FilterPropertyEnum.AMERICAS,
+    },
+    {
+      name: 'Europe',
+      filterProperty: FilterPropertyEnum.EUROPE,
+    },
+    {
+      name: 'Africa',
+      filterProperty: FilterPropertyEnum.AFRICA,
+    },
+    {
+      name: 'Asia',
+      filterProperty: FilterPropertyEnum.ASIA,
+    },
+    {
+      name: 'Oceania',
+      filterProperty: FilterPropertyEnum.OCEANIA,
+    },
   ];
+
+  const onClickFilterListItem = (obj: FilterItem) => {
+    // dispatch(setSort(obj));
+    setOpenFilter(false);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: globalThis.MouseEvent) => {
+      if (
+        filterRef.current &&
+        !event.composedPath().includes(filterRef.current)
+      ) {
+        setOpenFilter(false);
+      }
+    };
+    //FIXME document
+    document.body.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.body.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
 
   //SORT
 
   const sortRef = useRef<HTMLDivElement>(null);
   const [open, setOpen] = useState(false);
-  // const sort = useAppSelector(selectSort)
+  const sort = useSelector(selectSort);
   const sortList: SortItem[] = [
     {
       name: 'population (DESC)',
@@ -46,8 +122,8 @@ function HomePage() {
   ];
 
   const onClickListItem = (obj: SortItem) => {
-    // dispatch(setSort(obj))
-    // setOpen(false);
+    dispatch(setSort(obj));
+    setOpen(false);
   };
 
   useEffect(() => {
@@ -64,6 +140,8 @@ function HomePage() {
     };
   }, []);
 
+  //NEW SORT
+
   ///DATA
   // const { items, status } = useAppSelector(selectPizzasData);
 
@@ -76,7 +154,7 @@ function HomePage() {
   return (
     <div>
       <div className="content__top">
-        <div className="categories">
+        {/* <div className="categories">
           <ul>
             {categories.map((categoryName, index) => (
               <li
@@ -89,7 +167,51 @@ function HomePage() {
               </li>
             ))}
           </ul>
+        </div> */}
+        {/* NEW FILTER */}
+        <div ref={filterRef} className="categories">
+          <div className="categories__label">
+            <svg
+              width="10"
+              height="6"
+              viewBox="0 0 10 6"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
+                fill="#2C2C2C"
+              />
+            </svg>
+            <b>Filter:</b>
+            <span onClick={() => setOpenFilter(!openFilter)}>
+              Filter prop{/* {sort.name} */}
+            </span>
+          </div>
+          {openFilter && (
+            <div className="filter__popup">
+              <ul>
+                {filterList.map((obj, index) => (
+                  <li
+                    key={index}
+                    onClick={() => onClickFilterListItem(obj)}
+                    // className={
+                    //   sort.sortProperty === obj.sortProperty ? 'active' : ''
+                    // }
+                  >
+                    {obj.name}
+                  </li>
+                ))}
+                {/* <li className="active">популярности</li>
+                <li>цене</li>
+                <li>алфавиту</li> */}
+              </ul>
+            </div>
+          )}
         </div>
+        {/* SEARCH */}
+        <Search />
+        {/* SORT */}
         <div ref={sortRef} className="sort">
           <div className="sort__label">
             <svg
@@ -105,9 +227,7 @@ function HomePage() {
               />
             </svg>
             <b>Sort by:</b>
-            <span onClick={() => setOpen(!open)}>
-              Sort prop{/* {sort.name} */}
-            </span>
+            <span onClick={() => setOpen(!open)}>{sort.name}</span>
           </div>
           {open && (
             <div className="sort__popup">
