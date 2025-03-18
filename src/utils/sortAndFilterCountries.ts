16c4fcc61fafde555dda40be10f37f59d1b34148
Alexandr Kabanau <Azraill1989@gmail.com>
2025-03-18 21:23:44 +0300

    fix: fix previous one

--- src/utils/sortAndFilterCountries.ts
+++ src/utils/sortAndFilterCountries.ts
@@ -188,3 +188,56 @@ export const { setFilter, setSort } = filterSlice.actions;
 
 export default filterSlice.reducer;
   */
+
+///////////////////////////////////////////
+
+/* import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../store/store';
+import './MainPage.css';
+import Item from '../components/Item';
+import { setShadow } from '../store/reducers/dataSlice';
+
+function MainPage() {
+  const data = useAppSelector((state) => state.data);
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    if (data.length) {
+      const timeoutId = setTimeout(() => {
+        dispatch(setShadow(''));
+      }, 3000);
+      return () => clearTimeout(timeoutId);
+    }
+  }, [data, dispatch]);
+  return (
+    <div className="main-page">
+      <p className="main-page-title">Types of form:</p>
+      <nav className="main-page-nav">
+        <Link to={'/hookForm'} className="navLink">
+          HookForm
+        </Link>
+        <Link to={'/uncontrollerForm'} className="navLink">
+          Uncontrolled Form
+        </Link>
+      </nav>
+      <div className="data-container">
+        {data.length ? (
+          data.map((item, i) => {
+            const hightLightIndex = data.length - 1;
+            return (
+              <Item item={item} key={i} i={i} highlight={hightLightIndex} />
+            );
+          })
+        ) : (
+          <div className="empty-state">
+            <p>Items not fond</p>
+            <p>Please choose type of form</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default MainPage;
+ */
